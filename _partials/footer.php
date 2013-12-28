d186e21e3fe982a992f77375194335851ad5334f
John Crossley <hello@phpcodemonkey.com>
2013-12-28 15:20:17 +0000

    Cleaning up the styles and using implementing the changes for the new bootstrap

--- _partials/footer.php
+++ _partials/footer.php
@@ -1,24 +1,24 @@
-  <footer>
-    <div class="container">
-      <small>
-        &copy; <?=date('Y')?> - <a href="http://phpcodemonkey.com/" target="_blank">John Crossley</a> -
-        <a href="mailto:hello@phpcodemonkey.com" target="_blank">Contact</a>
-      </small>
-      <?php if (DEMO_MODE === true): ?>
-        <small class="pull-right text-danger">
-          <strong>DEMO MODE ENABLED</strong> v<?=system_version()?>
+<div class="container">
+    <hr>
+    <footer>
+        <small>
+            &copy; <?=date('Y')?> - <a href="http://phpcodemonkey.com/" target="_blank">John Crossley</a> -
+            <a href="mailto:hello@phpcodemonkey.com" target="_blank">Contact</a>
         </small>
-      <?php else: ?>
-        <small class="pull-right text-danger">
-          v<?=system_version()?>
-        </small>
-      <?php endif; ?>
-    </div><!--//.container-->
-  </footer>
-
-  <!-- Javascripts -->
-  <script src="<?=javascripts_path('jquery-2.0.0.min')?>"></script>
-  <script src="<?=javascripts_path('bootstrap')?>"></script>
-  <script src="<?=javascripts_path('app')?>"></script>
+        <?php if (DEMO_MODE === true): ?>
+            <small class="pull-right text-danger">
+                <strong>DEMO MODE ENABLED</strong> v<?=system_version()?>
+            </small>
+        <?php else: ?>
+            <small class="pull-right text-danger">
+                v<?=system_version()?>
+            </small>
+        <?php endif; ?>
+    </footer>
+</div>
+<!-- Javascripts -->
+<script src="<?=javascripts_path('jquery-2.0.0.min')?>"></script>
+<script src="<?=javascripts_path('bootstrap')?>"></script>
+<script src="<?=javascripts_path('app')?>"></script>
 
 <?php ob_end_flush(); ?>
--- _partials/header.php
+++ _partials/header.php
@@ -1,19 +1,21 @@
-<?php ob_start(); // Fixed made by KRauer ?>
+<?php ob_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <title> <?=$title?> &raquo; <?=system_name()?> </title>
+    <meta charset="utf-8">
+    <title> <?php echo $title; ?> &raquo; <?php echo system_name(); ?> </title>
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- Stylesheets -->
-  <link rel="stylesheet" href="<?=stylesheets_path('bootstrap-glyphicons')?>">
-  <link rel="stylesheet" href="<?=stylesheets_path('bootstrap')?>">
-  <link rel="stylesheet" href="<?=stylesheets_path('style')?>">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
 
-  <!-- Meta Information -->
-  <meta name="description" content="<?=meta_description()?>">
-  <meta name="author" content="<?=meta_author()?>">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="<?php echo stylesheets_path('main'); ?>">
+
+    <!-- Meta Information -->
+    <meta name="description" content="<?php echo meta_description(); ?>">
+    <meta name="author" content="<?php echo meta_author(); ?>">
 
 </head>
